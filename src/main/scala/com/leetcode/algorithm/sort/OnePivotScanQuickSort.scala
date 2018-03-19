98860bb49044e5a893e77c5663a2683f86a61c76
wangzunhui <wzh404@hotmail.com>
2018-03-19 22:41:29 +0800

    feat(Sort): one pivot quick sort
    - scan quick sort

--- src/main/scala/com/leetcode/algorithm/sort/OnePivotScanQuickSort.scala
+++ src/main/scala/com/leetcode/algorithm/sort/OnePivotScanQuickSort.scala
@@ -1,5 +1,44 @@
 package com.leetcode.algorithm.sort
 
 object OnePivotScanQuickSort {
+  def partition(list:Array[Int], left : Int, right:Int) : Int = {
+    val base:Int = list(left);
+    var i = left;
+    var j:Int = right;
 
+    while(i < j){
+      while(i < j && list(j) >= base){
+        j = j - 1;
+      }
+      if (i < j){
+        list(i) = list(j);
+        i = i + 1;
+      }
+
+      while(i < j && list(i) < base){
+        i = i + 1;
+      }
+      if (i < j){
+        list(j) = list(i);
+        j = j - 1;
+      }
+    }
+
+    list(i) = base;
+    i
+  }
+
+  def sort(list:Array[Int], left: Int, right:Int): Unit ={
+    if (left < right){
+      val i:Int = partition(list, left, right)
+      sort(list, left, i - 1)
+      sort(list, i + 1, right)
+    }
+  }
+
+  def main(args: Array[String]): Unit ={
+    val l:Array[Int] = Array(72,6,57,88,60,42,83,73,48,85)
+    sort(l, 0, l.size - 1)
+    println(l.mkString(","))
+  }
 }
