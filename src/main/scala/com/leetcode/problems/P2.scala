4224085bc401c3ca6e58327cae00bb56743e943f
Gitea <gitea@fake.local>
2018-03-20 15:27:35 +0800

    feat(leetcode): solution no.2 problem
    - add two numbers

--- src/main/scala/com/leetcode/problems/P2.scala
+++ src/main/scala/com/leetcode/problems/P2.scala
@@ -12,12 +12,39 @@ package com.leetcode.problems
   */
 object P2 {
   def addTwoNumbers(l1 : List[Int], l2: List[Int]) : List[Int] = {
-    val l: List[Int] = List()
+    var result: List[Int] = List()
+    var i = l1.length - 1;
+    var j = l2.length - 1;
+    var carry = 0;
 
-    l
+    while (j >=0 || i >= 0){
+      val v1:Option[Int] = l1.lift(i) match {
+        case None => Option(0)
+        case y:Option[Int] => y
+      }
+
+      val v2:Option[Int] = l2.lift(j) match {
+        case None => Option(0)
+        case y:Option[Int] => y
+      }
+
+      val sum = v1.get + v2.get + carry;
+      carry = sum / 10
+      val v = sum - carry * 10;
+      result = result :+ v;
+
+      i = i - 1;
+      j = j - 1;
+    }
+    if (carry == 1){
+      result = result :+ 1
+    }
+    result
   }
 
   def main(args: Array[String]) = {
-
+    val l1 = List(2,4,3);
+    val l2 = List(5,6,4);
+    println(addTwoNumbers(l1, l2))
   }
 }
