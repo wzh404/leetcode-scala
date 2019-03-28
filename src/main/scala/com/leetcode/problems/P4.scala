cab64bb5fce31390ff72741eb1b6b4694c009e25
wzh404 <wzh@hotmail.com>
2019-03-28 18:56:30 +0800

    feat: leetcode问题4实现

--- src/main/scala/com/leetcode/problems/P4.scala
+++ src/main/scala/com/leetcode/problems/P4.scala
@@ -1,5 +1,49 @@
 package com.leetcode.problems
 
 object P4 {
+  def mean(a: Array[Int], b: Array[Int]): Double = {
+    var a1: Int = 0
+    var a2: Int = a.length - 1
+    var b1: Int = 0
+    var b2: Int = b.length - 1
+    val even: Boolean = (a.length + b.length) % 2 == 0
+    val p1 = (a.length + b.length) / 2
 
+    while(true) {
+      var ma = a1 + (a2 - a1) / 2
+      var mb = b1 + (b2 - b1) / 2
+      var left : Int = ma + mb
+
+      if (p1 == left) {
+        if (a(ma) > b(mb)) {
+          println(b(mb))
+        } else {
+          println(a(ma))
+        }
+        return 0
+      }
+
+      if (p1 > left) {
+        if (a(ma) < b(mb)) {
+          a1 = ma
+        } else {
+          b1 = mb
+        }
+      } else if (p1 < left){
+        if (a(ma) < b(mb)) {
+          b2 = mb
+        } else {
+          a2 = ma
+        }
+      }
+    }
+    return 0;
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a: Array[Int] = Array(2, 4, 7, 11, 14, 20,23,34)
+    val b = Array(1, 5, 8, 9, 19, 21)
+
+    mean(a, b)
+  }
 }
