1eb9e97c903984eed2d5b9977d54dc484a446e9c
wzh404 <sw191402>
2020-12-13 14:39:19 +0800

    test(fn):  练习2.1
    尾递归函数获取第n个斐波那契数

--- src/main/scala/com/leetcode/fp/book/Fn2$1.scala
+++ src/main/scala/com/leetcode/fp/book/Fn2$1.scala
@@ -2,6 +2,14 @@ package com.leetcode.fp.book
 
 object Fn2$1 {
   def fib(n: Int): Int = {
-    0
+    @annotation.tailrec
+    def go(n: Int, current: Int, next: Int): Int = {
+      if (n == 0)
+        current
+      else
+        return go(n - 1, next, next+current)
+    }
+
+    go(n, 0, 1)
   }
 }
